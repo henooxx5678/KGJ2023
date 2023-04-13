a714e39df07f86102e240c06f41e4eac2e89c164
Steven Ma <steven.ma@kingmakergames.co>
2023-04-13 12:43:35 +0800

    lv2 done

--- Assets/_Main/Scripts/Global.cs
+++ Assets/_Main/Scripts/Global.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public static class Global {
@@ -11,6 +12,10 @@ public static class Global {
 
     public static void OnPlayerExitLevel (int levelNumber) {
         
+        if (levelNumber == 1) {
+            SceneManager.LoadScene("Apartment");
+        }
+
     }
 
     public static void OnLookSensitivityChanged () {
